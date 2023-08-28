35fb38a6dce0831022841199ddc01987c294741f
Mykhailo Chaban <76599514+MykhailoChaban@users.noreply.github.com>
2023-08-28 22:57:46 +0200

    Revert "Feature/shared button"

--- frontend/apps/stroytorg/src/app/app.component.ts
+++ frontend/apps/stroytorg/src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
-import { MainPageComponent } from '@frontend/stroytorg/main-page';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, MainPageComponent, MatIconModule],
+  imports: [RouterModule],
   selector: 'frontend-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
--- frontend/apps/stroytorg/src/app/app.routes.ts
+++ frontend/apps/stroytorg/src/app/app.routes.ts
@@ -1,7 +1,3 @@
 import { Route } from '@angular/router';
-export const appRoutes: Route[] = [
-    {
-        path: '',
-        loadChildren: () => import('@frontend/stroytorg/stroytorg-routing-shell').then(c => c.stroytorgRoutingShellRoutes),
-    },
-];
+
+export const appRoutes: Route[] = [];

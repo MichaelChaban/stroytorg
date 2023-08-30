ef904e756ed695fee66b4303be722ae08bb26224
Mykhailo Chaban <cmisa695@gmail.com>
2023-08-31 00:41:29 +0200

    Editing shared buttons

--- frontend/libs/shared/button/src/lib/button/button.component.ts
+++ frontend/libs/shared/button/src/lib/button/button.component.ts
@@ -5,11 +5,20 @@ import { ButtonStyleDirective } from './components/directives/buttonStyle.direct
 import { ButtonStyle, Icons } from '@frontend/shared/domain';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'stroytorg-button',
   standalone: true,
-  imports: [CommonModule, ButtonStyleDirective, MatIconModule, RouterModule],
+  imports: [
+    CommonModule,
+    ButtonStyleDirective, 
+    RouterModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatButtonModule
+  ],
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -19,14 +28,14 @@ export class ButtonComponent {
   buttonStyle = ButtonStyle.DEFAULT_BUTTON;
 
   @Input()
-  routerLink!: string;
+  routerLink?: string;
 
   @Input()
-  queryParams!: { [key: string]: any };
+  queryParams?: { [key: string]: any };
 
   @Input()
-  label!: string;
+  label?: string;
 
   @Input()
-  icon!: Icons;
+  icon?: Icons;
 }

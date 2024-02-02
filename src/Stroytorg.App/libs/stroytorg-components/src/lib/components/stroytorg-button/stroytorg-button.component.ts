0c712f1b3e03cc5b9e200159b86ff63e31ffa36d
Mykhailo Chaban <cmisa695@gmail.com>
2024-02-02 23:22:11 +0100

    changes: changing a button component, removing form component

--- src/Stroytorg.App/libs/stroytorg-components/src/lib/components/stroytorg-button/stroytorg-button.component.ts
+++ src/Stroytorg.App/libs/stroytorg-components/src/lib/components/stroytorg-button/stroytorg-button.component.ts
@@ -1,14 +1,46 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component, EventEmitter, HostListener,
+  Input, Output,
+  ViewEncapsulation,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonSize, ButtonType, CustomIcon } from './stroytorg-buttons.models';
+// import { APP_CONFIG_ASSETS_BASE_URL } from "@stroytorg/shared";
 
 @Component({
   selector: 'stroytorg-button',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './stroytorg-button.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['./stroytorg-button.component.scss'],
+  changeDetection: ChangeDetectionStrategy.Default,
+  encapsulation: ViewEncapsulation.None,
 })
 export class StroytorgButtonComponent {
+
+  // assets = inject(APP_CONFIG_ASSETS_BASE_URL);
+  @Input()
+  size = ButtonSize.normal as string;
+
+  @Input()
+  disabled!: boolean;
+
+  @Input()
+  type = ButtonType.default as string;
+
   @Input()
   title!: string;
+
+  @Input()
+  icon!: CustomIcon;
+
+  @Input() buttonType: 'submit' | 'button' = 'button' ;
+
+  @Output() clicked = new EventEmitter<void>();
+  
+  @HostListener('click')
+  clickedListen() {
+    this.clicked.emit();
+  }
 }

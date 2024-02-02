3bc94f66f95297c8b3e573fd380f5b07392969fd
Mykhailo Chaban <cmisa695@gmail.com>
2024-02-02 23:28:56 +0100

    refactor: refactoring

--- src/Stroytorg.App/libs/stroytorg-components/src/index.ts
+++ src/Stroytorg.App/libs/stroytorg-components/src/index.ts
@@ -1,5 +1,1 @@
-export * from './lib/components/stroytorg-navbar/stroytorg-navbar.models';
-export * from './lib/components/stroytorg-table/stroytorg-table.component';
-export * from './lib/components/stroytorg-base-form/stroytorg-base-form.component';
-export * from './lib/components/stroytorg-navbar/stroytorg-navbar.component';
-export * from './lib/components/stroytorg-button/stroytorg-button.component';
+export * from './lib/components/index';
---
+++ src/Stroytorg.App/libs/stroytorg-components/src/lib/components/index.ts
@@ -0,0 +1,6 @@
+export * from './stroytorg-base-form';
+export * from './stroytorg-button';
+export * from './stroytorg-checkbox';
+export * from './stroytorg-navbar';
+export * from './stroytorg-paginator';
+export * from './stroytorg-table';
